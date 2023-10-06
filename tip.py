4318d1d069e70bff8e80dbfd5aa4dd7e45302b8b
PearlRowe <146419328+PearlRowe@users.noreply.github.com>
2023-10-06 16:08:06 +0300

    Create tip.py

---
+++ tip.py
@@ -0,0 +1,8 @@
+bill =float(input("enter amount"))
+Percentage_tip=int(input(" tip is 10%,12% or 15%"))
+Number_of_people=int(input("Number of people splitting bill is"))
+Tip=bill*(Percentage_tip/100)
+Amount_to_be_paid=bill+Tip
+Amount_per_person=Amount_to_be_paid/Number_of_people
+K=round(Amount_per_person, 2)
+print("Each person pays:" , K)
